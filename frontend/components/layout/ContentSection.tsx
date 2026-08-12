3e0bd794df5c981cc086fead27c1b285627a2efe
rafaymirza3002 <rafaymirza3002@gmail.com>
2026-08-12 17:12:20 +0500

    Create dashboard layout

---
+++ frontend/components/layout/ContentSection.tsx
@@ -0,0 +1,7 @@
+export default function ContentSection() {
+  return (
+    <section className="min-h-[300px] rounded-lg border p-4">
+      Scholarship List Area
+    </section>
+  );
+}
---
+++ frontend/components/layout/StatsSection.tsx
@@ -0,0 +1,7 @@
+export default function StatsSection() {
+  return (
+    <section className="rounded-lg border p-4">
+      Stats Section
+    </section>
+  );
+}
